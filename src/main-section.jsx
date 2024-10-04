db38631d8d0e20da051b383dadb2ac4b8e129b42
JashwantChohan <116147134+JashwantChohan@users.noreply.github.com>
2024-10-04 17:17:04 +0500

    footer img updated

--- src/main-section.jsx
+++ src/main-section.jsx
@@ -43,7 +43,7 @@ const data = [
     img: 'https://depot.qodeinteractive.com/wp-content/uploads/2017/02/landing-m-slider-img-4.png'
   },
   {
-   img: 'https://depot.qodeinteractive.com/wp-content/uploads/2023/06/landing-m-slider-img-3.png'
+    img: 'https://depot.qodeinteractive.com/wp-content/uploads/2023/06/landing-m-slider-img-3.png'
   },
 
 
@@ -103,7 +103,7 @@ const MainSection = () => {
         variants={fadeIn("right", 0.2)}
         initial="hidden"
         whileInView={"show"}
-        viewport={{once: false, amount:0.1}}
+        viewport={{ once: false, amount: 0.1 }}
 
         className="w-screen text-center ">
         <h1 className="text-black text-5xl py-9  ">DEPOT</h1>
@@ -114,31 +114,31 @@ const MainSection = () => {
       <div>
 
         <div className=' grid sm:grid-cols-3 gap-12 min-h-[px] mt-32 mx-20 '>
- 
-          <motion.div
-           variants={fadeIn("up", 0.2)}
-           initial="hidden"
-           whileInView={"show"}
-           viewport={{once: false, amount:0.8}}
-          
-          className='cursor-pointer'><img className='min-h-[200px]  hover:shadow-lg  overflow-hidden transition duration-300 ease-in-out hover:bg-gray-200 transform hover:-translate-y-5 hover:scale-105'src="https://depot.qodeinteractive.com/wp-content/uploads/2017/02/landing-port1-img1.jpg" alt="" /></motion.div>
 
           <motion.div
-           variants={fadeIn("up", 0.2)}
-           initial="hidden"
-           whileInView={"show"}
-           viewport={{once: false, amount:0.8}}
-          
-          className=' cursor-pointer'><img className='min-h-[200px]  hover:shadow-lg  overflow-hidden transition duration-300 ease-in-out hover:bg-gray-200 transform hover:-translate-y-5 hover:scale-105' src="https://depot.qodeinteractive.com/wp-content/uploads/2017/02/landing-port1-img2.jpg" href="https://depot.qodeinteractive.com/minimal-home/" alt="" /></motion.div>
-          
+            variants={fadeIn("up", 0.2)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.8 }}
+
+            className='cursor-pointer'><img className='min-h-[200px]  hover:shadow-lg  overflow-hidden transition duration-300 ease-in-out hover:bg-gray-200 transform hover:-translate-y-5 hover:scale-105' src="https://depot.qodeinteractive.com/wp-content/uploads/2017/02/landing-port1-img1.jpg" alt="" /></motion.div>
+
           <motion.div
-           variants={fadeIn("up", 0.2)}
-           initial="hidden"
-           whileInView={"show"}
-           viewport={{once: false, amount:0.8}}
-          
-          className=' cursor-pointer'><img className='min-h-[200px]  hover:shadow-lg  overflow-hidden transition duration-300 ease-in-out hover:bg-gray-200 transform hover:-translate-y-5 hover:scale-105' src="https://depot.qodeinteractive.com/wp-content/uploads/2017/02/landing-port1-img3.jpg" alt="" /></motion.div>
-          
+            variants={fadeIn("up", 0.2)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.8 }}
+
+            className=' cursor-pointer'><img className='min-h-[200px]  hover:shadow-lg  overflow-hidden transition duration-300 ease-in-out hover:bg-gray-200 transform hover:-translate-y-5 hover:scale-105' src="https://depot.qodeinteractive.com/wp-content/uploads/2017/02/landing-port1-img2.jpg" href="https://depot.qodeinteractive.com/minimal-home/" alt="" /></motion.div>
+
+          <motion.div
+            variants={fadeIn("up", 0.2)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.8 }}
+
+            className=' cursor-pointer'><img className='min-h-[200px]  hover:shadow-lg  overflow-hidden transition duration-300 ease-in-out hover:bg-gray-200 transform hover:-translate-y-5 hover:scale-105' src="https://depot.qodeinteractive.com/wp-content/uploads/2017/02/landing-port1-img3.jpg" alt="" /></motion.div>
+
         </div>
 
 
@@ -149,7 +149,7 @@ const MainSection = () => {
           viewport={{ once: false, amount: 0.8 }}
 
           className='grid sm:grid-cols-3 gap-12 min-h-[px] bg-slate-100 my-10 mx-20'>
-          <div className=' cursor-pointer'><img className='min-h-[200px]  hover:shadow-lg  overflow-hidden transition duration-300 ease-in-out hover:bg-gray-200 transform hover:-translate-y-5 hover:scale-105' src="https://depot.qodeinteractive.com/wp-content/uploads/2017/02/landing-port1-img4.jpg"alt="" /></div>
+          <div className=' cursor-pointer'><img className='min-h-[200px]  hover:shadow-lg  overflow-hidden transition duration-300 ease-in-out hover:bg-gray-200 transform hover:-translate-y-5 hover:scale-105' src="https://depot.qodeinteractive.com/wp-content/uploads/2017/02/landing-port1-img4.jpg" alt="" /></div>
           <div className=' cursor-pointer'><img className='min-h-[200px]  hover:shadow-lg  overflow-hidden transition duration-300 ease-in-out hover:bg-gray-200 transform hover:-translate-y-5 hover:scale-105' src="https://depot.qodeinteractive.com/wp-content/uploads/2017/02/landing-port1-img5.jpg" alt="" /></div>
           <div className=' cursor-pointer'><img className='min-h-[200px]  hover:shadow-lg  overflow-hidden transition duration-300 ease-in-out hover:bg-gray-200 transform hover:-translate-y-5 hover:scale-105' src="https://depot.qodeinteractive.com/wp-content/uploads/2017/02/landing-port1-img6.jpg" alt="" /></div>
         </motion.div>
@@ -184,13 +184,13 @@ const MainSection = () => {
 
       <div className='bg-slate-100 grid sm:grid-cols-2 gap-2'>
         <motion.div
-         variants={fadeIn("right", 0.2)}
-         initial="hidden"
-         whileInView={"show"}
-         viewport={{ once: false, amount: 0.8 }}
-        
-        
-        className='bg-slate-100 min-h-[500px] grid-cols-3 py-10 px-24 text-gray-400'>
+          variants={fadeIn("right", 0.2)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.8 }}
+
+
+          className='bg-slate-100 min-h-[500px] grid-cols-3 py-10 px-24 text-gray-400'>
           <h1 className='text-2xl leading-9 tracking-wide my-5 mx-0 text-black font-semibold uppercase'>
             A Large Collection of Practical Inner Pages
           </h1>
@@ -208,13 +208,13 @@ const MainSection = () => {
 
         <div className='pt-16 group  hover:shadow-lg  overflow-hidden transition duration-300 ease-in-out  transform hover:-translate-y-5 hover:scale-105'>
           <motion.div
-           variants={fadeIn("left", 0.2)}
-           initial="hidden"
-           whileInView={"show"}
-           viewport={{once: false, amount:0.4}}
-          
-          
-          style={{ backgroundImage: `url(${slides[currentIndex].url})` }} className='h-full w-full bg-slate-100 bg-cover relative duration-500'>
+            variants={fadeIn("left", 0.2)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.4 }}
+
+
+            style={{ backgroundImage: `url(${slides[currentIndex].url})` }} className='h-full w-full bg-slate-100 bg-cover relative duration-500'>
             {/* left-arrow */}
             <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-black cursor-pointer'>
               <BsChevronCompactLeft onClick={prevSlide} size={30} />
@@ -229,26 +229,26 @@ const MainSection = () => {
       </div>
 
       <motion.div
-       variants={fadeIn("up", 0.2)}
-          initial="hidden"
-          whileInView={"show"}
-          viewport={{ once: false, amount: 0.7 }}
-      
-      
-      className=' justify-center text-center  mt-3 h-[300px]'>
+        variants={fadeIn("up", 0.2)}
+        initial="hidden"
+        whileInView={"show"}
+        viewport={{ once: false, amount: 0.7 }}
+
+
+        className=' justify-center text-center  mt-3 h-[300px]'>
         <h6 className='pt-20 tracking-widest pb-7 uppercase font-semibold ' >Blog & Portfolio</h6>
         <h1 className='text-gray-400 text-2xl tracking-widest'>Show off your portfolio and blog posts using <br /> Depot’s creative and stylish templates.</h1>
       </motion.div>
 
       <div>
-        <motion.div 
-        
-        variants={fadeIn("up", 0.2)}
-        initial="hidden"
-        whileInView={"show"}
-        viewport={{ once: false, amount: 0.5 }}
-        
-        className='grid sm:grid-cols-3 gap-12 min-h-[px]  mx-20'>
+        <motion.div
+
+          variants={fadeIn("up", 0.2)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.5 }}
+
+          className='grid sm:grid-cols-3 gap-12 min-h-[px]  mx-20'>
           <div className=' cursor-pointer'><img className='min-h-[200px]  hover:shadow-lg  overflow-hidden transition duration-300 ease-in-out hover:bg-gray-200 transform hover:-translate-y-5 hover:scale-105' src="https://depot.qodeinteractive.com/wp-content/uploads/2017/02/landing-port2-img1.jpg" alt="" /></div>
           <div className=' cursor-pointer'><img className='min-h-[200px]  hover:shadow-lg  overflow-hidden transition duration-300 ease-in-out hover:bg-gray-200 transform hover:-translate-y-5 hover:scale-105' src="https://depot.qodeinteractive.com/wp-content/uploads/2017/02/landing-port2-img2.jpg" alt="" /></div>
           <div className=' cursor-pointer'><img className='min-h-[200px]  hover:shadow-lg  overflow-hidden transition duration-300 ease-in-out hover:bg-gray-200 transform hover:-translate-y-5 hover:scale-105' src="https://depot.qodeinteractive.com/wp-content/uploads/2017/02/landing-port2-img3.jpg" alt="" /></div>
@@ -259,8 +259,8 @@ const MainSection = () => {
           initial="hidden"
           whileInView={"show"}
           viewport={{ once: false, amount: 0.5 }}
-        
-        className='grid sm:grid-cols-3 gap-12 min-h-[px] bg-slate-100 my-10 mx-20'>
+
+          className='grid sm:grid-cols-3 gap-12 min-h-[px] bg-slate-100 my-10 mx-20'>
           <div className=' cursor-pointer'><img className='min-h-[200px]  hover:shadow-lg  overflow-hidden transition duration-300 ease-in-out hover:bg-gray-200 transform hover:-translate-y-5 hover:scale-105' src="https://depot.qodeinteractive.com/wp-content/uploads/2017/02/landing-port2-img4.jpg" alt="" /></div>
           <div className=' cursor-pointer'><img className='min-h-[200px]  hover:shadow-lg  overflow-hidden transition duration-300 ease-in-out hover:bg-gray-200 transform hover:-translate-y-5 hover:scale-105' src="https://depot.qodeinteractive.com/wp-content/uploads/2017/02/landing-port2-img5.jpg" alt="" /></div>
           <div className=' cursor-pointer'><img className='min-h-[200px]  hover:shadow-lg  overflow-hidden transition duration-300 ease-in-out hover:bg-gray-200 transform hover:-translate-y-5 hover:scale-105' src="https://depot.qodeinteractive.com/wp-content/uploads/2017/02/landing-port2-img6-1.jpg" alt="" /></div>
@@ -271,8 +271,8 @@ const MainSection = () => {
           initial="hidden"
           whileInView={"show"}
           viewport={{ once: false, amount: 0.5 }}
-        
-        className='grid sm:grid-cols-3 gap-12 min-h-[px] bg-slate-100 my-10 mx-20'>
+
+          className='grid sm:grid-cols-3 gap-12 min-h-[px] bg-slate-100 my-10 mx-20'>
           <div className=' cursor-pointer'><img className='min-h-[200px]  hover:shadow-lg  overflow-hidden transition duration-300 ease-in-out hover:bg-gray-200 transform hover:-translate-y-5 hover:scale-105' src="https://depot.qodeinteractive.com/wp-content/uploads/2017/02/landing-port2-img7.jpg" alt="" /></div>
           <div className=' cursor-pointer'><img className='min-h-[200px]  hover:shadow-lg  overflow-hidden transition duration-300 ease-in-out hover:bg-gray-200 transform hover:-translate-y-5 hover:scale-105' src="https://depot.qodeinteractive.com/wp-content/uploads/2017/02/landing-port2-img8.jpg" alt="" /></div>
           <div className=' cursor-pointer'><img className='min-h-[200px]  hover:shadow-lg  overflow-hidden transition duration-300 ease-in-out hover:bg-gray-200 transform hover:-translate-y-5 hover:scale-105' src="https://depot.qodeinteractive.com/wp-content/uploads/2017/02/landing-port2-img9.jpg" alt="" /></div>
@@ -283,8 +283,8 @@ const MainSection = () => {
           initial="hidden"
           whileInView={"show"}
           viewport={{ once: false, amount: 0.5 }}
-        
-        className=' justify-center text-center  mt-3 h-[300px]'>
+
+          className=' justify-center text-center  mt-3 h-[300px]'>
           <h6 className='pt-20 tracking-widest text-2xl pb-7 uppercase font-semibold ' >Fully Responsive Layouts</h6>
           <h1 className='text-gray-400'>Depot is made with mobile in mind, meaning that every page is fully responsive and ready for high-density retina <br /> screens. Simply scan the QR codes bellow to visit Depot’s homepages on your mobile device.</h1>
         </motion.div>
@@ -294,10 +294,10 @@ const MainSection = () => {
             variants={fadeIn("right", 0.2)}
             initial="hidden"
             whileInView={"show"}
-            viewport={{ once: false, amount: 0.1 }} 
-          
-          
-          className=''>
+            viewport={{ once: false, amount: 0.1 }}
+
+
+            className=''>
             <Slider {...settings}>
               {data.map((d) => (
                 <div className=' h-[450px] border-black text-black rounded-xl *:'>
@@ -316,10 +316,16 @@ const MainSection = () => {
           initial="hidden"
           whileInView={"show"}
           viewport={{ once: false, amount: 0.1 }}
-        
-        className='mt-36 mx-8 bg '><img className='  overflow-hidden transition duration-300 ease-in-out  transform' src="./src/assets/last-div-img.png" alt="" /></motion.div>
+
+          className='mt-36 mx-8 bg '><img className='  overflow-hidden transition duration-300 ease-in-out  transform' src="./src/assets/last-div-img.png" alt="" /></motion.div>
       </div>
-      <div className='border-2 '><img src="./src/assets/footer.png" alt="" /></div>
+      <div className=''>
+        <img src="https://depot.qodeinteractive.com/wp-content/uploads/2017/02/landing-client-1h.png" alt="" />
+        <img src="https://depot.qodeinteractive.com/wp-content/uploads/2017/02/landing-client-2h.png" alt="" />
+        <img src="https://depot.qodeinteractive.com/wp-content/uploads/2017/02/landing-client-3h.png" alt="" />
+        <img src="https://depot.qodeinteractive.com/wp-content/uploads/2017/02/landing-client-4h.png" alt="" />
+        <img src="https://depot.qodeinteractive.com/wp-content/uploads/2017/02/landing-client-5h.png" alt="" />
+      </div>
     </div >
   );
 };
