de765a3fbf575042b826eed62714dd5991c14f8a
JashwantChohan <116147134+JashwantChohan@users.noreply.github.com>
2024-10-04 17:12:09 +0500

    images updated

--- src/main-section.jsx
+++ src/main-section.jsx
@@ -32,7 +32,7 @@ const data = [
     img: 'https://depot.qodeinteractive.com/wp-content/uploads/2017/02/landing-m-slider-img-2.png'
   },
   {
-    img: 'https://depot.qodeinteractive.com/wp-content/uploads/2017/02/landing-m-slider-img-1.png'
+    img: 'https://depot.qodeinteractive.com/wp-content/uploads/2017/02/landing-m-slider-img-6.png'
   },
 
 
@@ -43,7 +43,7 @@ const data = [
     img: 'https://depot.qodeinteractive.com/wp-content/uploads/2017/02/landing-m-slider-img-4.png'
   },
   {
-   img: 'https://depot.qodeinteractive.com/wp-content/uploads/2017/02/landing-m-slider-img-10.png'
+   img: 'https://depot.qodeinteractive.com/wp-content/uploads/2023/06/landing-m-slider-img-3.png'
   },
 
 
@@ -58,7 +58,7 @@ const data = [
   },
 
   {
-    img: 'https://depot.qodeinteractive.com/wp-content/uploads/2017/02/landing-m-slider-img-10.png'
+    img: 'https://depot.qodeinteractive.com/wp-content/uploads/2017/02/landing-m-slider-img-12.png'
   },
   {
     img: 'https://depot.qodeinteractive.com/wp-content/uploads/2017/02/landing-m-slider-img-11.png'
